050d9b49c5a314440ebc30761ae0546632bd2ef5
HairyFotr <hairyfotr@gmail.com>
2012-05-09 20:59:29 +0200

    blocks

--- cshell.sh
+++ cshell.sh
@@ -1,34 +1,82 @@
 echo "" > .buffer
 echo "" > .includes
+echo "" > .funcs # todo :)
+
+cnt=0
+lvl=0
 start="main() {"
 end="}"
+kind="code"
 
 while [ 1 = 1 ]
 do
+if [ $lvl == 0 ]
+then
+    echo -n "> "
+else
+    for i in `seq 1 $lvl`;
+    do
+        echo -n ". "
+    done
+fi
+
 read input
+
 if [ "`echo $input | grep ^#include`" == "" ]
 then
+    if [ "`echo $input | grep {`" != "" ]
+    then
+        kind="block"
+        let lvl=lvl+1
+    fi
     echo "$input" >> .buffer
+    let cnt=cnt+1
+
+
+    if [ "`echo $input | grep }`" != "" ]
+    then
+        let lvl=lvl-1
+        if [ "$lvl" == "0" ]
+        then
+            kind="code"
+        fi
+    fi
 else
+    kind="include"
     echo "$input" >> .includes
 fi
 
-
-cat .includes > .code.c
-echo "$start" >> .code.c
-cat .buffer >> .code.c
-echo "$end" >> .code.c
-
-gcc .code.c -o .codeout
-if [ "$?" == "0" ]
+if [ "$kind" != "block" ]
 then
-    ./.codeout
-else
-    echo .code.c
-    sed -i "`cat .buffer | wc -l` d" .buffer
+    cat .includes > .code.c
+    echo "$start" >> .code.c
+    cat .buffer >> .code.c
+    echo "$end" >> .code.c
+
+    gcc .code.c -o .out
+    if [ "$?" == "0" ]
+    then
+        ./.out
+    else
+        echo .code.c
+        if [ "$kind" == "include" ]
+        then
+            f=".includes"
+        else
+            f=".buffer"
+        fi
+
+        for i in `seq 0 $cnt`;
+        do
+            sed -i "`cat .buffer | wc -l` d" $f
+        done
+        kind="code"
+    fi
+
+    cnt=0
+    lvl=0
+    cat .buffer | grep -v ^puts | grep -v ^printf > .buffer2
+    cat .buffer2 > .buffer
+    echo
 fi
-
-cat .buffer | grep -v ^puts | grep -v ^printf > .buffer2
-cat .buffer2 > .buffer
-
 done
