92d5272eafea012c11155a58fb2cebf0e7ab19bd
HairyFotr <hairyfotr@gmail.com>
2012-05-09 23:34:22 +0200

    funcs, better output

--- cshell.sh
+++ cshell.sh
@@ -1,15 +1,29 @@
-echo "" > .buffer
-echo "" > .includes
-echo "" > .funcs # todo :)
+config=".cshell"
+incl=".incl"
+func=".func"
+main=".main"
+
+cat $config | grep "//incl" > $incl 2> /dev/null
+cat $config | grep "//func" > $func 2> /dev/null
+cat $config | grep "//main" > $main 2> /dev/null
+
+type="(void|bool|char|int)"
+vardecl() { echo "$1" | egrep "^$type[ a-zA-Z0-9-]+" | grep "="; }
+fundecl() { echo "$1" | egrep "^$type[ a-zA-Z0-9-]+" | grep "("; }
 
 cnt=0
 lvl=0
 start="main() {"
+out=$main
 end="}"
-kind="code"
 
+progout=.output
+outskip=0
+
+# interpreter loop
 while [ 1 = 1 ]
 do
+
 if [ $lvl == 0 ]
 then
     echo -n "> "
@@ -22,61 +36,68 @@ fi
 
 read input
 
-if [ "`echo $input | grep ^#include`" == "" ]
+if [ "`echo $input | grep ^#include`" != "" ]
 then
-    if [ "`echo $input | grep {`" != "" ]
-    then
-        kind="block"
-        let lvl=lvl+1
-    fi
-    echo "$input" >> .buffer
+    out=$incl
+    echo "Include directive"
+else
     let cnt=cnt+1
 
-
+    if [ "`echo $input | grep {`" != "" ]
+    then
+        let lvl=lvl+1
+    fi
     if [ "`echo $input | grep }`" != "" ]
     then
         let lvl=lvl-1
-        if [ "$lvl" == "0" ]
-        then
-            kind="code"
-        fi
     fi
-else
-    kind="include"
-    echo "$input" >> .includes
+
+    if [ "`vardecl \"$input\"`" != "" ]
+    then
+        echo "Variable declaration"
+    fi
+
+    if [ "`fundecl \"$input\"`" != "" ]
+    then
+        echo "Function declaration"
+        out=$func
+    fi
 fi
 
-if [ "$kind" != "block" ]
+echo "$input" >> $out
+
+if [ $lvl == 0 ]
 then
-    cat .includes > .code.c
+    echo > .code.c
+    cat $incl >> .code.c
+    cat $func >> .code.c
     echo "$start" >> .code.c
-    cat .buffer >> .code.c
+    cat $main >> .code.c
     echo "$end" >> .code.c
 
     gcc .code.c -o .out
-    if [ "$?" == "0" ]
+    if [ $? == 0 ]
     then
-        ./.out
+        ./.out &> $progout
+        let take=`cat $progout | wc -l`-outskip
+        if [ $take -lt 1 ]
+        then
+            take=0
+        fi
+        tail -n $take $progout
+        let outskip=`cat $progout | wc -l`
     else
         echo .code.c
-        if [ "$kind" == "include" ]
-        then
-            f=".includes"
-        else
-            f=".buffer"
-        fi
 
         for i in `seq 1 $cnt`;
         do
-            sed -i "`cat .buffer | wc -l` d" $f
+            sed -i "`cat $out | wc -l` d" $out
         done
-        kind="code"
     fi
 
+    out=$main
     cnt=0
     lvl=0
-    cat .buffer | grep -v ^puts | grep -v ^printf > .buffer2
-    cat .buffer2 > .buffer
     echo
 fi
 done
